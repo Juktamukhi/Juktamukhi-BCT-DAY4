a58b65353b77dc03de93161f2ca5698bc88678d6
Juktamukhi <juktamukhidey@gmail.com>
2025-02-28 14:37:26 +0530

    Add files via upload

---
+++ set intersection,union.py
@@ -0,0 +1,10 @@
+s1={1,2,3,5,6,7,8,9}
+s2={1,2,7,8,9}
+I=s1.intersection(s2)
+print(I)
+U=s1.union(s2)
+print(U)
+Sub=s1.issubset(s2)
+print(Sub)
+Super=s2.issuperset(s1)
+print(Super)
---
+++ set.py
@@ -0,0 +1,10 @@
+s={1,2,3}
+l={1,3,5,7}
+s.update(l)
+print(s)
+s.add(8)
+print(s)
+s.discard(7)
+print(s)
+s.remove(5)
+print(s)
